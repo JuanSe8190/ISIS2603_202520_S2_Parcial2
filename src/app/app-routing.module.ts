55757dc80bfb295eb88468eb567d863c0fef62fb
Juancho-bas <js.avendanor1@uniandes.edu.co>
2025-11-20 07:32:57 -0500

    avance mas de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'movie', pathMatch: 'full' },
+  {
+    path: 'movie',
+    loadChildren: () =>
+      import('./movie/movie.module').then((m) => m.MovieModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { ActorModule } from '../actor/actor.module';
+import { MovieRoutingModule } from './movie-routing/movie-routing.module';
 
 @NgModule({
   declarations: [MovieListComponent, MovieDetailComponent],
-  imports: [CommonModule, ActorModule],
+  imports: [CommonModule, ActorModule,MovieRoutingModule],
   exports: [MovieListComponent, MovieDetailComponent],
 })
 export class MovieModule {}
